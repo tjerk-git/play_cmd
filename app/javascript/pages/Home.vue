5d3c10656e5285ebe4a29d4e3fd0e9ec04bc5023
Nordy Vlasman <nordy@sigbar.nl>
2021-09-28 23:08:58 +0200

    Inertia init and form movement

---
+++ app/javascript/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1 class="text-2xl text-center">Home</h1>
+    <h2 class="text-xl text-center">This is a Vue page rendered with Inertiajs</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
