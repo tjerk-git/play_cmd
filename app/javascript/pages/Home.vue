a50cca636545e7a72fe49d091189c9ec9113f193
Nordy Vlasman <nordy@sigbar.nl>
2021-10-01 14:13:41 +0200

    Example logout button

--- app/javascript/pages/Home.vue
+++ app/javascript/pages/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+      <inertia-link :href="$routes.logout()">Log out</inertia-link>
     <h1 class="text-2xl text-center">Home</h1>
     <h2 class="text-xl text-center">This is a Vue page rendered with Inertiajs</h2>
   </div>
--- app/javascript/pages/Post/Show.vue
+++ app/javascript/pages/Post/Show.vue
@@ -2,6 +2,14 @@
     <div>
         {{ newCreated }}
         {{ post }}
+                <inertia-link
+                  class="block w-full text-left px-6 py-2 hover:bg-indigo-600 hover:text-white"
+                  :href="$routes.destroy_user_session()"
+                  method="delete"
+                  as="button"
+                >
+                  Logout
+                </inertia-link>
     </div>
 </template>
 
