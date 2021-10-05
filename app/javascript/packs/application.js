cb6a774b540b7342fe63a908f0d13c0d1fcbe651
Nordy Vlasman <nordy@sigbar.nl>
2021-10-05 17:03:49 +0200

    Added confetti and skills for seeds (commented out because long file)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "stylesheets/application"
+import './components/confetti'
 
 Rails.start()
 Turbolinks.start()
