0d389ba1b1bf017f4c27eb7fece8539132c3693a
Nordy Vlasman <nordy@sigbar.nl>
2021-10-05 16:17:32 +0200

    Tailwind wasnt using config file

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
-module.exports = function(api) {
+module.exports = function (api) {
   var validEnv = ['development', 'test', 'production']
   var currentEnv = api.env()
   var isDevelopmentEnv = api.env('development')
@@ -54,21 +54,11 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-proposal-private-methods',
-        {
-          loose: true
-        }
-      ],
-      [
-        '@babel/plugin-proposal-private-property-in-object',
-        {
-          loose: true
-        }
-      ],
-      [
         '@babel/plugin-transform-runtime',
         {
-          helpers: false
+          helpers: false,
+          regenerator: true,
+          corejs: false
         }
       ],
       [
@@ -76,7 +66,8 @@ module.exports = function(api) {
         {
           async: false
         }
-      ]
+      ],
+      ['@babel/plugin-proposal-private-methods', { loose: true }]
     ].filter(Boolean)
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#F7F7F7'
+      }
+    },
   },
   variants: {
     extend: {
