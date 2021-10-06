6668af6ea1947e7d88656b3bc72acc988aea4b51
Nordy Vlasman <nordy@sigbar.nl>
2021-10-06 10:57:14 +0200

    Mail env

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -3,12 +3,12 @@ class CommentMailer < ApplicationMailer
 
   def feedback_email
     @comment = params[:comment]
+    @link_to_post = post_path(@comment.post.slug)
     if @comment.user.name
         @commenter = @comment.user.name
     else
         @commenter = @comment.user.email
     end
-
     mail(to: @comment.post.user.email, subject: 'Someone left some feedback for you!')
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module PlayCmd
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.action_mailer.default_url_options = { host: ENV['APP_URL'] }
+
     Dotenv::Railtie.load
     # Configuration for the application, engines, and railties goes here.
     #
