86b7c1257f72d75890d4fa260ae175c13210e1d3
Nordy Vlasman <nordy@sigbar.nl>
2021-10-07 14:10:03 +0200

    turbo turbo 🔥

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "stylesheets/application"
 import './components/confetti'
+import "@hotwired/turbo-rails"
 
 Rails.start()
 Turbolinks.start()
