8e8ea0f3f5299ffa204755bef11c1561e3f23acc
Tjerk Dijkstra <tjerk@hey.com>
2021-10-12 13:21:15 +0200

    added name

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,6 +1,7 @@
 Aws.config.update({
   region: 'eu-west-1',
   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
+  name: ENV['S3_BUCKET_NAME']
 })
 
 S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
