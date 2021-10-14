6ffe0e1ab9f43f54bb45e1db95a103b920ab3767
Nordy Vlasman <nordy@sigbar.nl>
2021-10-14 21:03:14 +0200

    Its something

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,11 @@
 class RegistrationsController < Devise::RegistrationsController
-  layout 'auth'
+  def new
+    self.resource = build_resource
+    render layout: "auth"
+  end
 
   protected
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
-
 end
