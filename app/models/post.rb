a991a0ce40534029c6efac079dd0d1eae611cb01
Tjerk Dijkstra <tjerk@hey.com>
2021-10-15 11:32:24 +0200

    relations and troubles

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
 
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   has_many :likes, dependent: :delete_all
   has_many :comments, dependent: :delete_all
 
