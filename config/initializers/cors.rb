bc347a810ba19297041bb1ba5cbb28932e5fca89
Tjerk Dijkstra <tjerk@hey.com>
2021-10-15 10:00:25 +0200

    s3 fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://playcmd.s3-website.eu-west-1.amazonaws.com'
+      origins 'https://playcmd.s3.eu-west-1.amazonaws.com/'
       resource '*', headers: :any, methods: [:post, :put]
     end
   end
