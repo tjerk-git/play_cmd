9c89e015390144c1a2590df35990cc15c84eb03f
Tjerk Dijkstra <tjerk@hey.com>
2021-10-18 23:40:18 +0200

    trying to fix the links

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -1,10 +1,10 @@
+include Rails.application.routes.url_helpers
+
 class CommentMailer < ApplicationMailer
   default template_path: "mailers/#{self.name.underscore}"
 
   def feedback_email
     @comment = params[:comment]
-    post_link = ENV['APP_URL'] + @comment.post.title, "/post/" + @comment.post.slug 
-    
     if @comment.user.name
         @commenter = @comment.user.name
     else
--- app/mailers/highlight_mailer.rb
+++ app/mailers/highlight_mailer.rb
@@ -1,9 +1,10 @@
+include Rails.application.routes.url_helpers
+
 class HighlightMailer < ApplicationMailer
   default template_path: "mailers/#{self.name.underscore}"
 
   def highlight_email
     @highlight = params[:highlight]
-    post_link = ENV['APP_URL'] + @highlight.post.title, "/post/" + @highlight.post.slug
     mail(to: @highlight.post.user.email, subject: 'YOU GOT THE SPOTLIGHT!')
   end
 end
