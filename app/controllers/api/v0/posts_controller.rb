c399e295812799ae661ee1158323d9580583aa56
Nordy Vlasman <nordy@sigbar.nl>
2021-10-18 16:04:32 +0200

    Added user to post

--- app/controllers/api/v0/posts_controller.rb
+++ app/controllers/api/v0/posts_controller.rb
@@ -3,7 +3,6 @@ module Api
         class PostsController < ApiController
             def index
                 total = (params[:total] || 10).to_i
-
                 @posts = Post.includes(:user).order(created_at: :desc).take(total)
                 render "api/v0/posts/index", formats: :json
             end
--- app/views/api/v0/posts/index.json.jbuilder
+++ app/views/api/v0/posts/index.json.jbuilder
@@ -1,3 +1,4 @@
 json.array! @posts do |post|
     json.partial! "api/v0/posts/post", post: post
+    json.partial! "api/v0/shared/user", user: post.user
 end
