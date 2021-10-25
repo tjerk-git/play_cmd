c0bd2f4fd65b9c7fede9f34b05a9be3ee5ed49aa
Nordy Vlasman <nordy@sigbar.nl>
2021-10-25 15:24:23 +0200

    Small fix and replaced share button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -11,8 +6,7 @@ import "stylesheets/application"
 import './components/confetti'
 import './components/syntax-highlight'
 import "@hotwired/turbo-rails"
-import Alpine from 'alpinejs'
-import collapse from '@alpinejs/collapse'
+
 require("trix")
 require("@rails/actiontext")
 
@@ -20,9 +14,6 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-window.Alpine = Alpine
-Alpine.plugin(collapse)
-Alpine.start()
 
 let __SkipConfirmation = false;
 
