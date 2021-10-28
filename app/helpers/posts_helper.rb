3e9699f299f2a91f6bef3c8500eafe911edd0f87
Nordy Vlasman <nordy@sigbar.nl>
2021-10-29 00:20:59 +0200

    LAYOUT AND DARKMODE

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -5,10 +5,22 @@ module PostsHelper
     end
 
     def is_your_field(user, post)
-        return (user.tags & post.tags).any? 
+        return (user.tags & post.tags).any?
     end
 
     def is_your_post(post)
         return current_user == post.user
+    end
+
+    def get_cover(post)
+        return rails_blob_url(post.cover_image)
+    end
+
+    def get_avatar(post)
+        if post.user.avatar.attached?
+            return rails_blob_url(post.user.avatar)
+        else
+            return "https://ui-avatars.com/api/?name=" + post.user.avatar + ";color=7F9CF5&amp;background=EBF4FF"
+        end
     end
 end
