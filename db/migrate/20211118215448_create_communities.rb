7be7ca15f0509d4c1920352dc4f6cbb9d5634567
Nordy Vlasman <nordy@sigbar.nl>
2021-11-18 23:14:57 +0100

    [FEATURE] Basic community migration

---
+++ db/migrate/20211118215448_create_communities.rb
@@ -0,0 +1,25 @@
+class CreateCommunities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :communities do |t|
+      t.string :title, null: false
+      t.string :slug, null: false
+
+      t.string :small_title, null: false
+      t.string :community_avatar, null: false, default: "blob-1.png"
+
+      t.text :summary, null: true
+
+      t.integer :community_owner, null: false # Uncomment when you want creator to be the owner
+      t.integer :created_by, null: false
+
+      # Optional stuff (ready for discussion)
+
+      # t.string :category, null: true
+      # t.string :bg_color_hex, null: true, default: "#fff"
+      # t.string :text_color_hex, null: true, default: "#000"
+      # t.text :wiki_markdown, null: true
+
+      t.timestamps
+    end
+  end
+end
